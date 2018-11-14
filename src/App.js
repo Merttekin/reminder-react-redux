355a2f28d3d4dc406923efbd61fb92e453e4bacf
unknown <merttekin94@gmail.com>
2018-11-14 23:11:48 +0300

    all project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,97 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { addReminder, deleteReminder, clearReminders } from './actions';
+import moment from 'moment';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+      dueDate: ''
+    }
+  }
+
+  addReminder() {
+    console.log('duedate', this.state.dueDate);
+    this.props.addReminder(this.state.text, this.state.dueDate);
+  }
+
+  deleteReminder(id) {
+    this.props.deleteReminder(id);
+  }
+
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group col-sm-4">
+        {
+          reminders.map(reminder => {
+            return (
+              <li key={reminder.id} className="list-group-item" >
+                <div className="list-item">
+                  <div>{reminder.text}</div>
+                  <em>{moment(new Date(reminder.dueDate)).fromNow()}</em>
+                </div>
+                <div
+                  className="list-item delete-button"
+                  onClick={() => this.deleteReminder(reminder.id)}
+                >
+                  &#x2715;
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+
   render() {
+    console.log('this.props', this.props);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="title">Reminder</div>
+        <div className="form-inline">
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="I have to..."
+              onChange={event => this.setState({ text: event.target.value })}
+            />
+            <input
+              className="form-control"
+              type="datetime-local"
+              onChange={event => this.setState({ dueDate: event.target.value })}
+            />
+          </div>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => this.addReminder()}
           >
-            Learn React
-          </a>
-        </header>
+            Add Reminder
+        </button>
+        </div>
+        {this.renderReminders()}
+        <div
+        className="btn btn-danger"
+        onClick={() => this.props.clearReminders()}
+        >
+        Clear Reminders
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  console.log('state', state);
+  return {
+    reminders: state
+  }
+}
+
+export default connect(mapStateToProps, { addReminder, deleteReminder, clearReminders })(App);
---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDERS } from '../constants';
+
+export const addReminder = (text, dueDate) => {
+    const action = {
+        type: ADD_REMINDER,
+        text,
+        dueDate
+    }
+    console.log('action in reminder', action);
+    return action;
+}
+
+export const deleteReminder = (id) =>{
+    const action = {
+        type: DELETE_REMINDER,
+        id
+    }
+    console.log('delete aciton', action);
+    return action;
+}
+
+export const clearReminders = () =>{
+    return{
+        type: CLEAR_REMINDERS
+    }
+}
